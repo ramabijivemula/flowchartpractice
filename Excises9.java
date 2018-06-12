0b258d8e07cd6a56b5fbdba3c2fc91731c862d77
Rama <rama.ganjikunta@gmail.com>
2018-06-12 14:05:20 -0400

    my all programs

---
+++ Excises9.java
@@ -0,0 +1,8 @@
+//Write a Java program to compute the specified expressions and print the output
+public class Excises9 {
+	 public static void main(String[] arg) {
+
+	        System.out.println((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+	    }
+	}
+
